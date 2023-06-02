9d56c22f9aaa00994aa1ba2dfd0bccabc6deb769
ronna767 <133634559+ronna-N@users.noreply.github.com>
2023-06-02 21:17:00 +0700

    learn how to use Click a DOM element.
    cy.get('.btn').click() // Click on button
    cy.focused().click() // Click on el with focus
    cy.contains('Welcome').click() // Click on first el containing 'Welcome'

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,15 @@
 describe("home page", () => {
   it("passes", () => {
     cy.visit("https://example.cypress.io")
+  });
+  it("check h1 contain correct text", () => {
+    cy.visit("https://example.cypress.io/")
+    cy.get("h1").contains("Kitchen Sink")
+  });
+  it("check button contain correct text", () => {
+    cy.visit("https://example.cypress.io/")
+    cy.contains('Commands').click()
+    cy.contains('Querying').click()
+    cy.get("h1").contains("Querying")
   })
 })
